214f4f37dfb735e93824e8604ffa598a6032a1dc
aeser9o1 <aashiskumar986@gmail.com>
2019-04-12 12:48:44 +0530

    footer done

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,14 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { MatCardModule, MatToolbarModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatCardModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class DashboardModule { }
